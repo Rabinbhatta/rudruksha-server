9bee5f70cedf63f47bfae02336d5d29c21d52402
Rabinbhatta <rabinbhattarai646@gmail.com>
2024-12-21 03:14:09 +0545

    post login

--- controller/get.js
+++ controller/get.js
@@ -15,11 +15,24 @@ export const getUsers = async (req, res) => {
 
 export const getProducts = async (req, res) => {
   try {
-    const users = await Product.find(); // Await the query
-    // if (!users || users.length === 0) {
-    //   return res.status(404).json({ msg: "No data found" }); // Use 404 for empty data
-    // }
-    return res.status(200).json({ users }); // Use 200 for success
+    const products = await Product.find(); // Await the query
+    if (!products || products.length === 0) {
+      return res.status(404).json({ msg: "No data found" }); // Use 404 for empty data
+    }
+    return res.status(200).json({ products }); // Use 200 for success
+  } catch (error) {
+    return res.status(500).json({ msg: error.message }); // 500 for server error
+  }
+};
+
+export const getProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const product = await Product.findById(id); // Await the query
+    if (!product) {
+      return res.status(404).json({ msg: "No data found" }); // Use 404 for empty data
+    }
+    return res.status(200).json({product}); // Use 200 for success
   } catch (error) {
     return res.status(500).json({ msg: error.message }); // 500 for server error
   }
--- router/auth.js
+++ router/auth.js
@@ -4,7 +4,7 @@ import { register,login,deleteUser } from "../controller/auth.js"
 const router = express.Router()
 
 router.post("/register",register)
-router.get("/login",login)
+router.post("/login",login)
 router.delete("/deleteUser/:id",deleteUser)
 
 export default router
