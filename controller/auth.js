f5234ffd5fe204a74e94599dc1f7d50a67946d5b
Rabinbhatta <rabinbhattarai646@gmail.com>
2025-01-10 16:26:37 +0545

    auth header fix

--- controller/auth.js
+++ controller/auth.js
@@ -27,45 +27,55 @@ export const register = async(req,res)=>{
     }
 }
 
-export const login = async(req,res)=>{
+export const login = async (req, res) => {
     try {
-       const {email , password} = req.body;
-       const user = await  User.findOne({email});
-       if(!user){
-           res.status(404).json({error :"User not found!!"})
-
-       }
-       const cart = await Cart.findOne({ userId: user._id });
-       let productDetails = [];
-       if (cart) {
-         productDetails = await Promise.all(
-            cart.products.map(async (item) => {
-              const product = await Product.findById(item.productId);
-              if (product) {
-                return {
-                  ...product.toObject(), // Convert product document to a plain object
-                  quantity: item.quantity,
-                  totalPrice: item.total,
-                };
-              }
-              return null;
-            })
-          );
-       }
-     
-       // Fetch all product details in parallel
-      
-       const isMatch = await bcrypt.compare(password,user.password)
-       if(!isMatch){
-           res.status(404).json({error :"Wrong password!!"})
-       }else{
-       const token =  jwt.sign(user.id,process.env.JWT_KEY)
-
-       res.status(200).json({jwt:token,user:{name:user.fullName,email:user.email,id:user._id,cart:productDetails}});}    
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+  
+      if (!user) {
+        return res.status(404).json({ error: "User not found!" });
+      }
+  
+      const cart = await Cart.findOne({ userId: user._id });
+      let productDetails = [];
+  
+      if (cart) {
+        productDetails = await Promise.all(
+          cart.products.map(async (item) => {
+            const product = await Product.findById(item.productId);
+            if (product) {
+              return {
+                ...product.toObject(),
+                quantity: item.quantity,
+                totalPrice: item.total,
+              };
+            }
+            return null;
+          })
+        );
+      }
+  
+      const isMatch = await bcrypt.compare(password, user.password);
+  
+      if (!isMatch) {
+        return res.status(404).json({ error: "Wrong password!" });
+      }
+  
+      const token = jwt.sign({ id: user._id }, process.env.JWT_KEY);
+      return res.status(200).json({
+        jwt: token,
+        user: {
+          name: user.fullName,
+          email: user.email,
+          id: user._id,
+          cart: productDetails,
+        },
+      });
     } catch (error) {
-       res.status(404).json({error :error.message})
+      return res.status(500).json({ error: error.message });
     }
-}
+  };
+  
 
 export const deleteUser= async(req,res)=>{
     try {
