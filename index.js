e707e2d8e2efa0a6f9b297cc90168c99aa639dfe
Rabinbhatta <rabinbhattarai646@gmail.com>
2025-01-12 13:40:29 +0545

    react url

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors({
-    origin: 'http://localhost:3000', // Your React app domain
+    origin: 'https://rudraksha-theta.vercel.app/', // Your React app domain
 }));
 app.use(fileUpload({
     useTempFiles:true
