e098df3f4cd547444021e606d722da9e58c14a75
Rabinbhatta <rabinbhattarai646@gmail.com>
2025-01-31 22:33:06 +0545

    Category model

---
+++ models/category.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Name is required",
+    minlength: [2, "Too short"],
+    maxlength: [32, "Too long"],
+  },
+});
