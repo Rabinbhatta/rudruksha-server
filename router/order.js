743aacb7dd4e891655f3ca851b9ce3c14a3585b4
Rabinbhatta <rabinbhattarai646@gmail.com>
2025-10-24 23:37:56 +0545

    events done

--- router/order.js
+++ router/order.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.post('/create', jwt_verify, createOrder);
 router.delete('/delete/:id', deleteOrder);
 router.patch('/update/:id', editOrder);
-router.get('/user', getOrdersByUserId);
+router.get('/user',jwt_verify, getOrdersByUserId);
 
 
 export default router;
