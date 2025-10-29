64dfcb586702274de82af5cb8632387ee85d68b5
Rabinbhatta <rabinbhattarai646@gmail.com>
2025-10-29 22:08:33 +0545

    product

--- router/product.js
+++ router/product.js
@@ -4,6 +4,7 @@ import {
   deleteProduct,
   editProduct,
   searchProduct,
+  toggleProduct
 } from "../controller/product.js";
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.post("/create", createProduct);
 router.delete("/delete/:id", deleteProduct);
 router.patch("/update/:id", editProduct);
 router.get("/search", searchProduct);
+router.patch("/toggle/:id", toggleProduct);
 
 export default router;
