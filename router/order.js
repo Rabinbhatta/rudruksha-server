3970f51575fcf02dee6fc704e9c9aace5479c148
Rabinbhatta <rabinbhattarai646@gmail.com>
2025-11-01 08:28:32 +0545

    order api done

--- router/order.js
+++ router/order.js
@@ -4,17 +4,24 @@ import {
   deleteOrder,
   editOrder,
   getOrdersByUserId,
-  deleteOrdersByUserId
+  deleteOrdersByUserId,
+  getAllOrders,
+  getOrderById,
+  updateOrderStatus,
+  updatePaymentStatus
 } from '../controller/order.js';
 import { jwt_verify } from '../middleware/verify.js';
 
 const router = express.Router();
 
 router.post('/create', jwt_verify, createOrder);
-router.delete('/delete/:id', deleteOrder);
-router.patch('/update/:id', editOrder);
+router.delete('/:id', deleteOrder);
 router.get('/user',jwt_verify, getOrdersByUserId);
 router.patch('/user/cancel/:orderId', jwt_verify, deleteOrdersByUserId);
-
+router.get("/all", getAllOrders)
+router.get(":id", getOrderById)
+router.put("/:id", editOrder)
+router.patch("/:id/status", updateOrderStatus)
+router.patch("/:id/payment",updatePaymentStatus)
 
 export default router;
